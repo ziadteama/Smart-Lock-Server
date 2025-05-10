a617242c52bf876951df1749aef910661fa0b199
Ziad Teama <ziadbasaria600@gmail.com>
2025-05-10 23:53:54 +0300

    require login

--- routes/pinRoutes.js
+++ routes/pinRoutes.js
@@ -2,20 +2,16 @@
 
 import { Router } from 'express';
 import * as pinController from '../controllers/pinController.js';
-// import requireLogin from '../middleware/requireLogin.js'; // ← Comment this out for now
+import requireLogin from '../middleware/requireLogin.js'; // ← Comment this out for now
 
 const router = Router();
 
-// router.post('/set', requireLogin, pinController.setPin);
-router.post('/set', pinController.setPin);
+router.post('/set', requireLogin, pinController.setPin);
 
-// router.post('/verify', requireLogin, pinController.verifyPin);
-router.post('/verify', pinController.verifyPin);
+router.post('/verify', requireLogin, pinController.verifyPin);
 
-// router.put('/update', requireLogin, pinController.updatePin);
-router.put('/update', pinController.updatePin);
+router.put('/update', requireLogin, pinController.updatePin);
 
-// router.delete('/delete', requireLogin, pinController.deletePin);
-router.delete('/delete', pinController.deletePin);
+router.delete('/delete', requireLogin, pinController.deletePin);
 
 export default router;
